ecb884c7bd25d810aa6ad34a6ea0655d1f14ab94
juanabibiloni <jbibiloni@udesa.edu.ar>
2026-03-12 15:46:36 -0300

    Update funciones_habitos.py

--- funciones_habitos.py
+++ funciones_habitos.py
@@ -1,4 +1,13 @@
 def registrar_habitos():
+    '''
+    Función que le pregunta al usuario actividades y las guarda en una lista.
+
+    Returns
+    -------
+    lista_act : lista
+        Lista de actividades.
+
+    '''
     agregar = input("Quiere agregar? ") 
     while True:
         if agregar == "si":
@@ -7,6 +16,33 @@ def registrar_habitos():
             lista_act.append(actividades)
             agregar = input("Quiere agregar? ")
         elif agregar == "no":
-            break
+            break  
+    return lista_act
         
-        
+
+
+
+def analizar_habitos(lista):
+    '''
+    Función que cuenta la cantidad de veces que aparece un elemento en una lista y los guarda en un diccionario.
+
+    Parameters
+    ----------
+    lista : lista
+        Lista de actividades.
+
+    Returns
+    -------
+    dicc_act : diccionario
+        Diccionario cuyas claves son las actividades y valores son la cantidad de veces que aparecían en la lista.
+
+    '''
+    dicc_act = {}
+    for actividad in lista:
+        if actividad not in dicc_act:
+            dicc_act[actividad]=1
+        else:
+            dicc_act[actividad]+=1
+    return dicc_act
+
+            
